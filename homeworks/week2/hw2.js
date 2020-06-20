7cc990955b09e1819fb05f24b76abb3e38833093
david-christian <a2266565693@gmail.com>
2020-06-21 03:21:23 +0800

    week2 完成

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,9 @@
-function capitalize(str) {
-  
+function capitalize (str) {
+  let result = ''
+	for (let i=1; i<str.length; i++) {
+		result += str[i]
+	}
+	return str[0].toUpperCase() + result
 }
 
 console.log(capitalize('hello'));
