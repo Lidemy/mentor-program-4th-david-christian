da0ec82e3fb23b53114c5f2a2f9ed2b854c4502b
david-christian <a2266565693@gmail.com>
2020-06-27 13:56:55 +0800

    week2 challenge 完成

---
+++ homeworks/week2/week2 challenge.js
@@ -0,0 +1,18 @@
+function search (arr, n) {
+  var L = 0
+  var R = arr.length -1
+  while (L <= R) {
+    var M = Math.floor((L+R)/2)
+    if (arr[M] === n) { 
+      return M
+    }
+    if (arr[M] > n) {
+      R = M - 1
+    } else {
+      L = M + 1
+    }
+  }
+  return -1
+}
+
+console.log (search([1, 3, 10, 14, 39], 14))
